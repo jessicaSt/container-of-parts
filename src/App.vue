9c2d13c909aa94c3252cadc27033170fb7f265d5
Jessica Staehler <jessykahst@gmail.com>
2023-01-31 21:31:56 -0300

    feat(#1):Adicionado o Tailwind, Criado a pagina Home

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+   <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+     <p class="text-3xl text-gray-700 font-bold mb-5">
+       Welcome!
+     </p>
+     <p class="text-gray-500 text-lg">
+       Vue and Tailwind CSS in action
+     </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<scrip lang="ts">
+export default {
+    
+}
+</scrip>
+<style lang="">
+    
+</style>
